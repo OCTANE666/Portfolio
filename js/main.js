05331faf842c638937f9540f60bd89868ab59bfc
Karthik-N-P <knp5826@gmail.com>
2022-10-30 22:32:02 +0530

    first commt

--- js/main.js
+++ js/main.js
@@ -21,3 +21,27 @@ tabsContainer.addEventListener("click", (event) =>{
 })
 
 })();
+
+(()=>{
+    const sections = document.querySelectorAll(".section");
+    sections.forEach((section) =>{
+        if(!section.classList.contains("active")){
+            section.classList.add("hide");
+        }
+    })
+})();
+
+/* navigation button */
+(()=>{
+  const hamburgerBtn =document.querySelector(".hamburger-btn"),
+  navMenu = document.querySelector(".nav-menu"),
+  closeNavBtn = navMenu.querySelector(".close-nav-menu");
+  hamburgerBtn.addEventListener("click",showNavMenu);
+  closeNavBtn.addEventListener("click",hideNavMenu);
+  function showNavMenu(){
+    navMenu.classList.toggle("open");
+  }
+  function hideNavMenu(){
+    navMenu.classList.remove("open");
+  }
+})();
